192b5f3dc233cf5d0ad54f6aa0876be3ddf41442
IGNOREDSOUL <xavhue04@gmail.com>
2023-04-18 02:01:10 +0930

    HeaderChecker

---
+++ header_checker.py
@@ -0,0 +1,17 @@
+import os
+
+def find_files_with_three_hashes(path):
+    file_paths = []
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.txt') or file.endswith('.c') or file.endswith('.h'):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    first_line = f.readline()
+                if first_line.startswith("/* ") or first_line.startswith("/**"):
+                    file_paths.append(file_path)
+
+    return file_paths
+
+print(find_files_with_three_hashes('./'))
